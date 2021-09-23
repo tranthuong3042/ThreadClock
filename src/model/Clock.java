9cf1cae25a3cb74d7e261ae04d54038f320eb7ec
Your Name <tranthuong3042@gmail.com>
2021-09-23 21:40:50 +0700

    ThreadClock

---
+++ src/model/Clock.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Clock {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Clock() {
+    }
+
+    public Clock(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+    
+}
